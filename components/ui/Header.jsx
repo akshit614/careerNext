90f9d49eae4bd1b1cb6c6860ee7853f9a6e9361b
akshit614 <akkisingh8266@gmail.com>
2025-03-17 23:21:57 +0530

    modified header

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,17 +1,32 @@
 import { SignInButton, SignedOut, SignUpButton, UserButton, SignedIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './button'
+import { LayoutDashboard } from 'lucide-react'
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 px-10'>
+      <nav className='container mx-auto px-4 h-16 flex justify-between items-center'>
+          <Link href={'/dashboard'}>
+          <Image src={'vercel.svg'} alt='Logo' width={200}  height={20} className='h-12 py-1 w-auto object-contain'/>
+          </Link>
+        <div className='flex'>
+        <SignedIn>
+          <Link href={'/'} className='px-4'> <Button> 
+            <LayoutDashboard/> Get Started
+            </Button> 
+          </Link>
+          <UserButton/>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        </div>
+      </nav>
+    </header>
   )
 }
 
