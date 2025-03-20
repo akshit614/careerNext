086e15d176eea7d8dd7aaed3e5f4f811832c0931
akshit614 <akkisingh8266@gmail.com>
2025-03-20 23:02:08 +0530

    Completed header

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -3,7 +3,18 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './button'
-import { LayoutDashboard } from 'lucide-react'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox } from 'lucide-react'
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} 
+  from './dropdown-menu'
+import { Graduate } from 'next/font/google'
+
 
 const Header = () => {
   return (
@@ -12,17 +23,58 @@ const Header = () => {
           <Link href={'/dashboard'}>
           <Image src={'vercel.svg'} alt='Logo' width={200}  height={20} className='h-12 py-1 w-auto object-contain'/>
           </Link>
-        <div className='flex'>
+        <div className='flex items-center space-x-2 md:space-x-5'>
         <SignedIn>
-          <Link href={'/'} className='px-4'> <Button> 
-            <LayoutDashboard/> Get Started
+          <Link href={'/'}> 
+          <Button> 
+            <LayoutDashboard/> 
+            <span className='hidden md:block'>Get Started</span> 
             </Button> 
           </Link>
-          <UserButton/>
+          
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+          <Button> 
+            <span className='hidden md:block'>Growth Tools</span> 
+            <ChevronDown/> 
+            </Button> 
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link href={'/'} className='flex gap-3 items-center'>
+              <FileText/> 
+              <span>Build Resume</span> 
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={'/'} className='flex gap-3 items-center'>
+                <PenBox/> 
+                <span>Cover letter</span> 
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={'/'} className='flex gap-3 items-center'>
+                <GraduationCap/> 
+                <span>Interview Prep</span> 
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UserButton
+          appearance={{elements : {
+            avatarBox : "w-10 h-10"
+          }}}
+          />
         </SignedIn>
+  
         <SignedOut>
-          <SignInButton />
-          <SignUpButton />
+          <SignInButton > 
+            <Button variant="outline">SignIn</Button> 
+          </SignInButton>
+          <SignUpButton > 
+            <Button variant="outline">Signup</Button> 
+          </SignUpButton>
         </SignedOut>
         </div>
       </nav>
