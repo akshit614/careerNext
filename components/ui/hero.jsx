a8b6aec9c150cafa5d5208965c0ee9482849c7e7
akshit614 <akkisingh8266@gmail.com>
2025-03-25 16:38:32 +0530

    completed hero section

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useRef } from 'react'
 import { Button } from './button'
 import Link from 'next/link'
 import Image from 'next/image'
 
 export const HeroSection = () => {
+
+    const imageRef = useRef(null);
+    useEffect(()=> {
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+
+            const scrollPostion = window.scrollY;
+            const scrollThreshold = 100;
+            if(scrollPostion > scrollThreshold) {
+                imageElement.classList.add("scrolled");
+            }else {
+                imageElement.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return ()=> window.removeEventListener("scroll", handleScroll);
+
+    }, [])
+
   return (
-    <section className='w-full pt-36 md:pt-48 pb-10'>
+    <section className='w-full pt-36 md:pt-40 pb-10'>
         <div className='space-y-6 text-center'>
             <div className='space-y-6 mx-auto'>
                 <h1 className='gradient gradient-title text-5xl md:text-6xl lg:text-7xl xl:text-8xl'>
@@ -22,9 +45,9 @@ export const HeroSection = () => {
                 </Link>
             </div>
 
-            <div>
-                <div>
-                    <Image src={"/banner3.jpeg"} alt='Banner Carrier-next ' width={1280} height={720}
+            <div className='hero-image-wrapper mt-5 md:mt-0 mb-5'>
+                <div ref={imageRef} className='hero-image'>
+                    <Image src={"/banner3.jpeg"} alt='Banner Carrier-next ' width={720} height={460}
                     className='rounded-lg shadow-2xl mx-auto border' priority/>
                 </div>
             </div>
