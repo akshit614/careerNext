790d4cc41424aef83ca4432059bf5382acf9003c
akshit614 <akkisingh8266@gmail.com>
2025-04-03 23:06:03 +0530

    created market features of dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,21 @@
+import { getIndustryInsights } from '@/actions/dashboard';
 import { getUserOnboardingStatus } from '@/actions/user';
 import { redirect } from 'next/navigation';
+import DashboardView from './_components/DashboardView';
+
 
 const IndustryInsightsPage = async() => {
 
-     const { isOnboarded } = await getUserOnboardingStatus();
-      if(!isOnboarded) {
-        redirect("/onboarding");
-      }
+  const insights = await getIndustryInsights();
+  const { isOnboarded } = await getUserOnboardingStatus();
+  if(!isOnboarded) {
+    redirect("/onboarding");
+  }
 
   return (
-    <div className=''>IndustryInsightsPage</div>
+    <div className='container mx-auto'>
+      <DashboardView insights={insights}/>
+    </div>
   )
 }
 
