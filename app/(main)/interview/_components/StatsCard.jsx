b6c64c7f11700789b73976451cfdc50b34e10ae9
akshit614 <akkisingh8266@gmail.com>
2025-04-12 23:30:53 +0530

    added stats

---
+++ app/(main)/interview/_components/StatsCard.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Brain, Trophy } from 'lucide-react';
+
+
+const StatsCard = ({assessments}) => {
+    
+    const averageScore = () => {
+        if(!assessments?.length) return 0;
+
+        const total = assessments.reduce(
+            (sum, assessment) => sum + assessment.quizScore, 0
+        );
+
+        return (total / assessments.length).toFixed(1);
+    }
+
+    const getLatestAssessments = () => {
+        if(!assessments?.length) return null;
+        return assessments[0];
+    }
+
+    const getTotalQuestion = () => {
+        if(!assessments?.length) return 0;
+
+        return assessments.reduce(
+            (sum, assessment) => sum + assessment.questions.length, 0
+        ); 
+    }
+
+  return (
+    <div className='grid gap-4 md:grid-cols-3'>
+        <Card >
+          <CardHeader className="flex flex-row items-center justify-between space-x-0 ">
+            <CardTitle className="text-sm font-medium">Average Score</CardTitle>
+            <Trophy className="h-4 w-4 text-yellow-200"/>
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>{averageScore()}%</div>
+            <p className='text-muted-foreground text-xs'>Accross all assessments</p>
+          </CardContent>
+        </Card>
+        
+        <Card >
+          <CardHeader className="flex flex-row items-center justify-between space-x-0 ">
+            <CardTitle className="text-sm font-medium">Questions Practiced</CardTitle>
+            <Brain className="h-4 w-4 text-green-500"/>
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>{getTotalQuestion()}</div>
+            <p className='text-muted-foreground text-xs'>Total questions</p>
+          </CardContent>
+        </Card>
+        
+        <Card >
+          <CardHeader className="flex flex-row items-center justify-between space-x-0 ">
+            <CardTitle className="text-sm font-medium">Latest Score</CardTitle>
+            <Trophy className="h-4 w-4 text-yellow-300"/>
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>{getLatestAssessments()?.quizScore.toFixed(1) || 0}%</div>
+            <p className='text-muted-foreground text-xs'>Most recent quiz</p>
+          </CardContent>
+        </Card>
+        
+    </div>
+  )
+}
+
+export default StatsCard
--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import StatsCard from './_components/StatsCard'
+import PerformanceChart from './_components/PerformanceChart'
+import QuizList from './_components/QuizList'
+import { getAssesment } from '@/actions/interview'
 
-const InterviewPage = () => {
+const InterviewPage = async() => {
+
+  const assessments = await getAssesment();
+
   return (
-    <div>InterviewPage</div>
+    <div>
+
+      <h1 className='text-6xl font-bold gradient-title mb-5'>Interview Preparation</h1>
+
+      <div>
+        <StatsCard assessments = {assessments} />
+        <PerformanceChart assessments = {assessments} />
+        <QuizList assessments = {assessments} />
+      </div>
+
+    </div>
   )
 }
 
