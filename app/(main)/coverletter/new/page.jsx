6918d9665847e09db1d2ecf5bef3a890fa80e9f6
akshit614 <akkisingh8266@gmail.com>
2025-04-23 16:31:59 +0530

    server action for creating cover letter

--- app/(main)/coverletter/new/page.jsx
+++ app/(main)/coverletter/new/page.jsx
@@ -1,8 +1,27 @@
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const newCoverLetter = () => {
   return (
-    <div>newCoverLetter</div>
+    <div className='container mx-auto space-y-4 py-2'>
+        <div className='flex flex-col space-y-2 mx-2'>
+            <Link href={'/coverletter'}>
+                <Button variant="link" className="gap-2 pl-0 cursor-pointer">
+                    <ArrowLeft className='h-4 w-4' />
+                    Back to all cover letters
+                </Button>
+            </Link>
+
+            <div>
+                <h1 className="text-6xl gradient-title font-bold">Create Cover Letter</h1>
+                <p className='text-muted-foreground'>Generate a tailored cover letter for your job application</p>
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
